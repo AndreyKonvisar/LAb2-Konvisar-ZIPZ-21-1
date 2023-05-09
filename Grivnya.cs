d6d0c353f725a5590be81c68e5657d785ab1343a
AndreyKonvisar <gomynkyl475@gmail.com>
2023-05-09 20:05:00 +0300

    Лабораторна на тему "Принципи програмування. SOLID"
    :)

---
+++ Grivnya.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+   
+    internal class Grivnya : Money
+    {
+        public Grivnya(int money, int coins) : base(money, coins)
+        {
+
+        }
+        public void GetMoneyAltogether()
+        {
+            Console.WriteLine($"Тимчасова цiна: {money}.{coins} гривень");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Reporting reporting = new Reporting();
+            Grivnya saltPrice = new Grivnya(10,20);
+            Dolar plasticPrice = new Dolar(5,10);
+            Product salt = new Product("сiль", saltPrice);
+            Product plastic = new Product("пластмаса", plasticPrice);
+
+            Grivnya decreaseMoney = new Grivnya(2,30);
+
+           
+            salt.ProductPrice.GetMoneyAltogether();
+            salt.decreasePrice(decreaseMoney, salt);
+            salt.ProductPrice.GetMoneyAltogether();
+
+            WareHouse saltWarehouse = new WareHouse("Сольвий", "кг", "06.05.2023", 0, salt);
+            WareHouse plastWarehouse = new WareHouse("Пластиковий", "м", "05.04.2023", 4, plastic);
+
+            saltWarehouse.removeProduct(salt);
+            saltWarehouse.addProduct(salt);
+            saltWarehouse.removeProduct(salt);
+            plastWarehouse.addProduct(plastic);
+
+            reporting.GetWarehouseBalances(saltWarehouse);
+            reporting.GetWarehouseBalances(plastWarehouse);
+            
+
+
+
+
+        }
+    }
+}
